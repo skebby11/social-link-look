5cacf53b22e02108f9793e5ff55793c58e1e63c5
BuildTools <unconfigured@null.spigotmc.org>
2020-05-17 02:30:48 +0200

    Initial commit

---
+++ sll-db.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function SLL_create_table() {
+	
+	global $wpdb;
+	
+	$SLL_table_name=$wpdb->prefix."sll_options";
+	
+	
+	
+	$SLL_query = "CREATE TABLE $SLL_table_name (
+		id int(10) NOT NULL AUTO_INCREMENT,
+		primary_title varchar (250)  DEFAULT '',
+		primary_meta_title varchar (250)  DEFAULT '',
+		primary_description varchar (250)  DEFAULT '',
+		og_type varchar (250)  DEFAULT '',
+		og_url varchar (250)  DEFAULT '',
+		og_title varchar (250)  DEFAULT '',
+		og_description varchar (250)  DEFAULT '',
+		og_image varchar (250)  DEFAULT '',
+		twitter_card varchar (250)  DEFAULT '',
+		twitter_url varchar (250)  DEFAULT '',
+		twitter_title varchar (250)  DEFAULT '',
+		twitter_description varchar (250)  DEFAULT '',
+		twitter_image varchar (250)  DEFAULT '',
+		PRIMARY KEY (id)
+	)";
+	
+	require_once(ABSPATH ."wp-admin/includes/upgrade.php");
+	dbDelta($SLL_query);
+}
+
+?>
